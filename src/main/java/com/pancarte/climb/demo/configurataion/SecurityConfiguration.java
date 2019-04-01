522ba4bb6739be0500f93e74e7d37e1ff95c12db
CactusDeMexico <33250840+CactusDeMexico@users.noreply.github.com>
2019-04-01 19:35:59 +0200

    user connection

--- src/main/java/com/pancarte/climb/demo/configurataion/SecurityConfiguration.java
+++ src/main/java/com/pancarte/climb/demo/configurataion/SecurityConfiguration.java
@@ -12,10 +12,12 @@ import org.springframework.security.web.authentication.rememberme.JdbcTokenRepos
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import javax.persistence.Table;
 import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
     @Autowired
@@ -24,14 +26,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     @Autowired
     private DataSource dataSource;
 
-    private final String USERS_QUERY = "select email, password, active from user where email=?";
-   // private final String ROLES_QUERY = "select u.email, r.role from user u inner join user_role ur on (u.id = ur.user_id) inner join role r on (ur.role_id=r.role_id) where u.email=?";
+    private final String USERS_QUERY = "select email, password, active from user1 where email=?";
+    private final String ROLES_QUERY = "select u.email, r.role from user1 u inner join user_role ur on (u.iduser = ur.user_id) inner join role r on (ur.role_id=r.role_id) where u.email=?";
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
                  auth.jdbcAuthentication()
                 .usersByUsernameQuery(USERS_QUERY)
-                //.authoritiesByUsernameQuery(ROLES_QUERY)
+                .authoritiesByUsernameQuery(ROLES_QUERY)
                 .dataSource(dataSource)
                 .passwordEncoder(bCryptPasswordEncoder);
     }
--- src/main/java/com/pancarte/climb/demo/controller/UserController.java
+++ src/main/java/com/pancarte/climb/demo/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.persistence.Table;
 import javax.validation.Valid;
 
 @Controller
